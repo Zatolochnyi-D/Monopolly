f83b930fa2226ab06203e89ef109a90dedc19574
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-03-13 18:55:12 +0200

    Created basic tiles and testing field

---
+++ Assets/Scripts/TileLogic.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileLogic : MonoBehaviour
+{
+    [SerializeField] private GameObject nextTile;
+
+    public GameObject NextTile { get => nextTile; }
+}
