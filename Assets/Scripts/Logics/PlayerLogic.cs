0a46855cab3e03a13646f2720af479175353b6b8
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-03-18 18:46:23 +0200

    Implemented advanced player positions. Now players won't stack on each other and will be visible all the time

--- Assets/Scripts/Logics/PlayerLogic.cs
+++ Assets/Scripts/Logics/PlayerLogic.cs
@@ -7,17 +7,29 @@ public class PlayerLogic : MonoBehaviour
     void Start()
     {
         currentTile = MapManager.Instance.StartTile;
-        transform.position = currentTile.GetPosition();
+        transform.position = currentTile.GetTemporalPosition();
+
+        TileLogic.PositionUpdated += OnPositionUpdated;
+    }
+
+    private void OnPositionUpdated(int id)
+    {
+        if (id == currentTile.TileID)
+        {
+            transform.position = currentTile.GetUpdatedPosition(this);
+        }
     }
 
     public void MovePlayer(int rolledNumber)
     {
+        currentTile.ReleasePlace(this);
+
         for (int i = 0; i < rolledNumber; i++)
         {
             currentTile = currentTile.NextTile;
         }
 
-        transform.position = currentTile.GetPosition();
+        transform.position = currentTile.TakePlace(this);
         currentTile.AlterPlayer(this);
     }
 }
--- Assets/Scripts/Logics/TileLogic.cs
+++ Assets/Scripts/Logics/TileLogic.cs
@@ -1,17 +1,82 @@
+using System;
+using System.Linq;
 using UnityEngine;
 
 public class TileLogic : MonoBehaviour
 {
+    public static event Action<int> PositionUpdated;
+
     [SerializeField] private int tileID;
     [SerializeField] private TileLogic nextTile;
     [SerializeField] private TileEffectSO effectSO;
+    [SerializeField] private Transform[] playerPositions;
+
+    private PlayerLogic[] players = new PlayerLogic[0];
+    private int tileCapacityLevel = 0;
 
     public int TileID => tileID;
     public TileLogic NextTile => nextTile;
 
-    public Vector3 GetPosition()
+    public Vector3 GetTemporalPosition()
     {
-        return transform.position + new Vector3(0, 0, -1);
+        return playerPositions[0].GetChild(0).position;
+    }
+
+    public Vector3 TakePlace(PlayerLogic player)
+    {
+        // look for place on tile for a new player
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i] == null)
+            {
+                // if place is empty, place new player here and return position
+
+                players[i] = player;
+                return playerPositions[tileCapacityLevel - 1].GetChild(i).position;
+            }
+        }
+
+        // if there is not enough rooms, raise tile capacity and place new player
+
+        tileCapacityLevel++;
+        PlayerLogic[] newPlayers = new PlayerLogic[tileCapacityLevel];
+        players.CopyTo(newPlayers, 0);
+
+        newPlayers[^1] = player;
+        players = newPlayers;
+        PositionUpdated.Invoke(tileID);
+        return playerPositions[tileCapacityLevel - 1].GetChild(tileCapacityLevel - 1).position;
+    }
+
+    public Vector3 GetUpdatedPosition(PlayerLogic player)
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i] == player)
+            {
+                return playerPositions[tileCapacityLevel - 1].GetChild(i).position;
+            }
+        }
+
+        throw new Exception("Something went wrong while updating players position");
+    }
+
+    public void ReleasePlace(PlayerLogic player)
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i] == player)
+            {
+                players[i] = null;
+                break;
+            }
+        }
+
+        if (players.All(x => x == null))
+        {
+            tileCapacityLevel = 0;
+            players = new PlayerLogic[tileCapacityLevel];
+        }
     }
 
     public void AlterPlayer(PlayerLogic player)
