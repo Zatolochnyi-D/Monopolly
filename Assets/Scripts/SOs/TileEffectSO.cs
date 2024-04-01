e743b4c18e481de57d6b6dfe8a1f49ab8974261f
Zatolochnyi-D <d.zatolochnyi@gmail.com>
2024-04-01 17:37:01 +0300

    TileEffectSO removed.

--- Assets/Scripts/SOs/TileEffectSO.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class TileEffectSO : ScriptableObject
-{
-    public abstract void AlterPlayer(PlayerLogic player);
-}
--- Assets/Scripts/SOs/VacationTileEffectSO.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "TileEffects/Vacation")]
-public class VacationTileEffectSO : TileEffectSO
-{
-    public override void AlterPlayer(PlayerLogic player)
-    { }
-}
